e6be39200663c1637ba2ceca8cdc7f70914611d4
Isaac Gomes Medeiros <isaac-gomes-medeiros@hotmail.com>
2019-05-03 07:48:44 -0300

    Add files via upload

--- Escultor/main.cpp
+++ Escultor/main.cpp
@@ -7,21 +7,6 @@ using namespace std;
 
 int main()
 {
-   //ALOCAÇÃO DINAMICA DA MATRIZ DE VOXELS
-
-    Voxel ***v;
-    int nx,ny,nz;
-
-    v = (Voxel ***) malloc(nz * sizeof(Voxel**));
-    v[0] =(Voxel **) malloc(nz*ny* sizeof(Voxel*));
-    v[0][0] = (Voxel *) malloc(nx*ny*nz*sizeof (Voxel));
-
-    for(int i=1; i<nz; i++){
-        v[i] = v[i-1] + ny;
-    }
-    for(int i=1; i<nz*ny; i++){
-        v[0][i] = v[0][i-1] + nx;
-    }
 
 
 
